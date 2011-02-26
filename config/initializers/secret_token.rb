5618099c4160efe68bad80702fa55b658f5c46aa
Sebastian <sebastian@precis-project.com>
2011-02-26 16:03:00 +0100

    rails 3.0.4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Precis::Application.config.secret_token = 'e9cc664b59a81dd44f467bcfc847f7ae1f5d040badf30ff77f2c9128779204b70d669f4799dc5b9790e4c008db0d9b4eb2779ea558d9999a68799e24e3ae45bc'
